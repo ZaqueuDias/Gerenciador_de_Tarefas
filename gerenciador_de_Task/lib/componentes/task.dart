d0fe8ce788b3b10f2cf55f80456872f60deedb57
Zaqueu Dias <zaqueudias980@gmail.com>
2024-06-04 12:19:14 -0300

    salvar

--- gerenciador_de_Task/lib/componentes/task.dart
+++ gerenciador_de_Task/lib/componentes/task.dart
@@ -11,12 +11,17 @@ class _TaskState extends State<Task> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(),
+      padding: EdgeInsets.only(left: 30, top: 10),
       child: Container(
-        width: 50,
-        height: 220,
+        width: 300,
+        height: 120,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5), color: Colors.blue),
+          border: Border.all(color: Colors.black12, width: 3),
+            borderRadius: BorderRadius.circular(15)),
+        child: Text(
+          '17:00',
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
       ),
     );
   }
--- gerenciador_de_Task/lib/telas/principal.dart
+++ gerenciador_de_Task/lib/telas/principal.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gerenciador_de_task/componentes/task.dart';
 
 class Tela_Principal extends StatefulWidget {
@@ -6,6 +8,19 @@ class Tela_Principal extends StatefulWidget {
 
   @override
   State<Tela_Principal> createState() => _Tela_PrincipalState();
+}
+
+List<Widget> criardado(int quanto, {Task? tarefa, Container? caixa}){
+  print('chamou');
+  List<Widget> dados = [];
+  for(int num = 0; num <= quanto; num++){
+    dados.add(Row(
+      children: [
+        Center(child: tarefa.runtimeType == Task? Task():Container(width: 20, height: 20, color: Colors.white,))
+      ],
+    ));
+  }
+  return dados;
 }
 
 class _Tela_PrincipalState extends State<Tela_Principal> {
@@ -16,7 +31,9 @@ class _Tela_PrincipalState extends State<Tela_Principal> {
         alignment: AlignmentDirectional.bottomEnd,
         children: [
           Container(
+            alignment: AlignmentDirectional.center,
             color: Colors.blue,
+            child: ListView.builder(scrollDirection: Axis.horizontal,itemCount: criardado(10).length,itemBuilder: (context, int){return criardado(10, caixa: Container())[int];}),
           ),
           Container(
             height: 530,
@@ -25,7 +42,12 @@ class _Tela_PrincipalState extends State<Tela_Principal> {
                   topLeft: Radius.circular(50), topRight: Radius.circular(50)),
               color: Colors.white,
             ),
-            child: ListView(children: [Task()],),
+            child: ListView.builder(
+              itemCount: criardado(10, tarefa: Task()).length,
+              itemBuilder: (context, int valor) {
+                return criardado(10, tarefa: Task())[valor];
+              },
+            ),
           )
         ],
       ),
