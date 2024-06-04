ee121da0e3d288a36126d93797fdd657a47b4d94
Zaqueu Dias <zaqueudias980@gmail.com>
2024-06-04 20:43:46 -0300

    adicionando calendario

--- gerenciador_de_Task/lib/main.dart
+++ gerenciador_de_Task/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciador_de_task/telas/calendario.dart';
 import 'package:gerenciador_de_task/telas/principal.dart';
 
 void main() {
-  runApp(Tela_Principal());
+  runApp(Calendario());
 }
 
